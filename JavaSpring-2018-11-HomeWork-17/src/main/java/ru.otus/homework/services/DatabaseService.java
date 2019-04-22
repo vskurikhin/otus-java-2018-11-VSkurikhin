7e89b56f3d15438b3dc93a23f442c90a2b295e99
Victor N. Skurikhin <vskurikhin@gmail.com>
2019-04-22 23:31:32 +0300

    Home work 17

---
+++ JavaSpring-2018-11-HomeWork-17/src/main/java/ru.otus/homework/services/DatabaseService.java
@@ -0,0 +1,63 @@
+package ru.otus.homework.services;
+
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.homework.models.Author;
+import ru.otus.homework.models.Book;
+import ru.otus.homework.models.Genre;
+import ru.otus.homework.models.Review;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DatabaseService
+{
+    Optional<Author> getAuthorById(long id);
+
+    Optional<Author> getAuthorByFirstNameAndLastName(String firstName, String lastName);
+
+    List<Author> getAllAuthors();
+
+    List<Author> getAuthorsForBookId(long id);
+
+    Author saveAuthor(Author author);
+
+    void removeAuthor(long id);
+
+    void addAuthorToBook(Author author, Book book);
+
+    long countBooksByAuthorId(long id);
+
+    Optional<Book> getBookById(long id);
+
+    List<Book> getAllBooks();
+
+    List<Book> getAllBooksByAuthorId(long id);
+
+    Book saveBook(Book book);
+
+    Book saveBookNewGenre(Book book, String genre);
+
+    void removeBook(long id);
+
+    Optional<Genre> getGenreById(long id);
+
+    Optional<Genre> getGenreByValue(String genre);
+
+    List<Genre> getAllGenres();
+
+    Genre saveGenre(Genre genre);
+
+    void removeGenre(long id);
+
+    long countReviewsByBookId(long id);
+
+    Optional<Review> getReviewById(long id);
+
+    List<Review> getAllReviews();
+
+    List<Review> getAllReviewsForBookById(long id);
+
+    Review saveReview(Review review);
+
+    void removeReview(long id);
+}
---
+++ JavaSpring-2018-11-HomeWork-17/src/test/java/ru/otus/outside/utils/TestData.java
@@ -0,0 +1,223 @@
+package ru.otus.outside.utils;
+
+import org.springframework.http.MediaType;
+import ru.otus.homework.models.*;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
+public class TestData
+{
+    public static String TEST = "test";
+
+    public static long TEST_ID = 13L;
+
+    public static Long TEST_LID = 13L;
+
+    public static String TEST_SID = "13";
+
+    public static int TEST_NUM = 3;
+
+    public static String TEST_FIRST_NAME = "test_first_name_13";
+
+    public static String TEST_LAST_NAME = "test_last_name_13";
+
+    public static String TEST_ISBN = "test_isbn_name_13";
+
+    public static String TEST_TITLE = "test_title_name_13";
+
+    public static String TEST_COPYRIGHT= "test_copyright_13";
+
+    public static String TEST_GENRE_NAME = "test_genre_13";
+
+    public static String TEST_COMMENT_NAME = "test_comment_13";
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
+        MediaType.APPLICATION_JSON.getType(),
+        MediaType.APPLICATION_JSON.getSubtype(),
+        Charset.forName("utf8")
+    );
+
+    public static Author createAuthor0()
+    {
+        Author result = new Author();
+        result.setId(0L);
+        result.setFirstName("test_first_name_0");
+        result.setLastName("test_last_name_0");
+
+        return result;
+    }
+
+    public static Author createAuthor1()
+    {
+        Author result = new Author();
+        result.setId(1L);
+        result.setFirstName("test_first_name_1");
+        result.setLastName("test_last_name_1");
+
+        return result;
+    }
+
+    public static Author createAuthor2()
+    {
+        Author result = new Author();
+        result.setId(2L);
+        result.setFirstName("test_first_name_2");
+        result.setLastName("test_last_name_2");
+
+        return result;
+    }
+
+    public static Genre createGenre0()
+    {
+        Genre result = new Genre();
+        result.setId(0L);
+        result.setValue("test_genre_0");
+
+        return result;
+    }
+
+    public static Genre createGenre1()
+    {
+        Genre result = new Genre();
+        result.setId(1L);
+        result.setValue("test_genre_1");
+
+        return result;
+    }
+
+    public static Genre createGenre2()
+    {
+        Genre result = new Genre();
+        result.setId(2L);
+        result.setValue("test_genre_2");
+
+        return result;
+    }
+
+    public static Book createBook0()
+    {
+        Book result = new Book();
+        result.setId(0L);
+        result.setIsbn("test_isbn_0");
+        result.setTitle("test_title_0");
+        result.setEditionNumber(TEST_NUM);
+        result.setCopyright("test_copyright_0");
+        result.setGenre(createGenre0());
+
+        return result;
+    }
+
+    public static Book createBook0(Genre genre)
+    {
+        Book result = new Book();
+        result.setId(0L);
+        result.setIsbn("test_isbn_0");
+        result.setTitle("test_title_0");
+        result.setEditionNumber(TEST_NUM);
+        result.setCopyright("test_copyright_0");
+        result.setGenre(genre);
+
+        return result;
+    }
+
+    public static Book createBook0(Genre genre, Author... authors)
+    {
+        Book result = new Book();
+        result.setId(0L);
+        result.setIsbn("test_isbn_0");
+        result.setTitle("test_title_0");
+        result.setEditionNumber(TEST_NUM);
+        result.setCopyright("test_copyright_0");
+        Arrays.stream(authors).forEach(a -> result.getAuthors().add(a));
+        result.setGenre(genre);
+
+        return result;
+    }
+
+    public static Book createBook1()
+    {
+        Book result = new Book();
+        result.setId(1L);
+        result.setIsbn("test_isbn_1");
+        result.setTitle("test_title_1");
+        result.setEditionNumber(TEST_NUM);
+        result.setCopyright("test_copyright_1");
+        result.setGenre(createGenre1());
+
+        return result;
+    }
+
+    public static Book createBook2()
+    {
+        Book result = new Book();
+        result.setId(2L);
+        result.setIsbn("test_isbn_2");
+        result.setTitle("test_title_2");
+        result.setEditionNumber(TEST_NUM);
+        result.setCopyright("test_copyright_2");
+        result.setGenre(createGenre2());
+
+        return result;
+    }
+
+    public static Review createReview0()
+    {
+        Review result = new Review();
+        result.setId(0L);
+        result.setReview("test_review_0");
+        result.setBook(createBook0());
+
+        return result;
+    }
+
+    public static Review createReview0(Book book)
+    {
+        Review result = new Review();
+        result.setId(0L);
+        result.setReview("test_review_0");
+        result.setBook(book);
+
+        return result;
+    }
+
+    public static Review createReview1()
+    {
+        Review result = new Review();
+        result.setId(1L);
+        result.setReview("test_review_1");
+        result.setBook(createBook1());
+
+        return result;
+    }
+
+    public static Review createReview1(Book book)
+    {
+        Review result = new Review();
+        result.setId(1L);
+        result.setReview("test_review_1");
+        result.setBook(book);
+
+        return result;
+    }
+
+    public static Review createReview2()
+    {
+        Review result = new Review();
+        result.setId(2L);
+        result.setReview("test_review_2");
+        result.setBook(createBook2());
+
+        return result;
+    }
+
+    public static Review createReview2(Book book)
+    {
+        Review result = new Review();
+        result.setId(2L);
+        result.setReview("test_review_2");
+        result.setBook(book);
+
+        return result;
+    }
+}
